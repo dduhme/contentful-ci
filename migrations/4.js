189fd6b068efc20815ddb12efad1366904df939e
David Duhme <david.duhme@mhp.com>
2020-12-02 14:04:34 +0100

    new content type author

---
+++ migrations/4.js
@@ -0,0 +1,15 @@
+module.exports = function runMigration(migration) {
+    const author = migration.createContentType('author', {
+        name: 'Author',
+        description: 'Blog post author'
+    });
+    
+    author.createField('firstname', {
+        name: 'Firstname',
+        type: 'Symbol'
+    });
+    author.createField('lastname', {
+        name: 'Lastname',
+        type: 'Symbol'
+    });
+  };
